dd6c3566c9db3cd84cf72edc47f9fbc85d5e5f0d
im7mortal <im7mortal@gmail.com>
2017-05-20 21:35:51 -0700

    cluster

--- python/cluster.py
+++ python/cluster.py
@@ -3,8 +3,13 @@ print(__doc__)
 import numpy as np
 from sklearn.cluster import MeanShift, estimate_bandwidth
 from sklearn.datasets.samples_generator import make_blobs
+
 centers = [[1, 1], [-1, -1], [1, -1]]
-X, _ = make_blobs(n_samples=10000, centers=centers, cluster_std=0.6)
+
+data_name = "data.csv"
+
+X = np.loadtxt(data_name, delimiter=',')
+
 # The following bandwidth can be automatically detected using
 bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=500)
 
@@ -26,10 +31,10 @@ plt.clf()
 
 colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
 for k, col in zip(range(n_clusters_), colors):
-    my_members = labels == k
-    cluster_center = cluster_centers[k]
-    plt.plot(X[my_members, 0], X[my_members, 1], col + '.')
-    plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
-             markeredgecolor='k', markersize=14)
+	my_members = labels == k
+	cluster_center = cluster_centers[k]
+	plt.plot(X[my_members, 0], X[my_members, 1], col + '.')
+	plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
+			 markeredgecolor='k', markersize=14)
 plt.title('Estimated number of clusters: %d' % n_clusters_)
 plt.show()
